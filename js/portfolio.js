052390e456e2b1e379d2d4ed8c0f3b0aae0f5fdc
RyanKim01 <kyhekm@hotmail.com>
2016-01-18 18:03:44 -0800

    FInished making project list page

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,24 @@
 $( document ).ready(function() {
-   alert('hello');
- });
+  $("#gallery").justifiedGallery({
+    rowHeight : 300,
+    // lastRow : 'justify',
+    margins : 10,
+    randomize: true
+  }).on('jg.complete', function () {
+      $('#gallery a').swipebox({
+        hideBarsDelay : false,
+        hideCloseButtonOnMobile : false
+      });
+  });
+
+  $("#gallery2").justifiedGallery({
+    rowHeight : 300,
+    margins : 10,
+    randomize: true
+  }).on('jg.complete', function () {
+      $('#gallery2 a').swipebox({
+        hideBarsDelay : false,
+        hideCloseButtonOnMobile : false
+      });
+  });
+});
